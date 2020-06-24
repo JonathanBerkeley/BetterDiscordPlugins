7cb8299dc20222415465efb17cd00c4edc321947
JonathanBerkeley <35268549+JonathanBerkeley@users.noreply.github.com>
2020-06-24 13:35:55 +0100

    Update Siphon.plugin.js

--- Siphon/Siphon.plugin.js
+++ Siphon/Siphon.plugin.js
@@ -1,4 +1,4 @@
-//META{"name":"Siphon","website":"https://github.com/JonathanBerkeley/BetterDiscordPlugins/","source":"https://github.com/JonathanBerkeley/BetterDiscordPlugins/blob/master/Siphon/Siphon.plugin.js"}*//
+//META{"name":"Siphon","website":"https://github.com/JonathanBerkeley/BetterDiscordPlugins/","source":"https://github.com/JonathanBerkeley/BetterDiscordPlugins/blob/master/Siphon/Siphon.plugin.js","donate":"https://paypal.me/shudworknow"}*//
 
 class Siphon {
     // Constructor
@@ -9,12 +9,15 @@ class Siphon {
     // Meta
     getName() { return "Siphon"; }
     getDescription() { return "Siphons text randomly from all your servers messages out through you into the chat you are currently looking at. (early DEV stage release)"; }
-    getVersion() { return "0.0.1"; }
+    getVersion() { return "0.0.2"; }
     getAuthor() { return "J.B"; }
 
     // Settings Panel
     getSettingsPanel() {
+
         return "<h1> Unused </h1>";
+
+
     }
 
     // Load/Unload
@@ -23,10 +26,9 @@ class Siphon {
     unload() { }
 
     // Events
-    onMessage() { };
-
     onSwitch() {
-        this.currentChannel = location.href.substring(location.href.lastIndexOf("/") + 1, location.href.length);
+        //var currentChannel = location.href.substring(location.href.lastIndexOf("/") + 1, location.href.length);
+        //console.log(currentChannel);
     };
 
     observer(e) {
@@ -34,38 +36,54 @@ class Siphon {
 
     // Start/Stop
     start() {
-        this.cancelPatch = BdApi.monkeyPatch(BdApi.findModuleByProps("dispatch"), "dispatch", {
-            before: (data) => {
-                var event = data.methodArguments[0];
-                var fStop = false; //unused
+        var chance = 10; //Change to change chance, higher means less likely
+        document.body.onkeyup = function (e) {
+            if (e.keyCode == 37) {
+                console.log(chance);
                 var lastMsg = "";
-                if (event.type === "MESSAGE_CREATE" && fStop === false && Math.floor((Math.random() * 25) + 1) === 2) { //Change the 25 to change chance, higher means less likely
-                    if (event.message.content != "") {
-                        if (event.message.content != lastMsg) {
-                            BdApi.findModuleByProps('enqueue').enqueue(
-                                {
-                                    message: {
-                                        channelId: this.currentChannel,
-                                        content: event.message.content,
-                                        nonce: BdApi.findModuleByProps('fromTimestamp').fromTimestamp(new Date),
+                this.cancelPatch = BdApi.monkeyPatch(BdApi.findModuleByProps("dispatch"), "dispatch", {
+                    before: (data) => {
+                        var event = data.methodArguments[0];
+                        var fStop = false; //unused
+                        if (event.type === "MESSAGE_CREATE" && fStop === false && Math.floor((Math.random() * chance) + 1) === 2) {
+                            if (event.message.content != lastMsg && event.message.content != "") {
+                                BdApi.findModuleByProps('enqueue').enqueue(
+                                    {
+                                        message: {
+                                            channelId: location.href.substring(location.href.lastIndexOf("/") + 1, location.href.length),
+                                            content: event.message.content,
+                                            nonce: BdApi.findModuleByProps('fromTimestamp').fromTimestamp(new Date),
+                                        },
+                                        type: 'send',
                                     },
-                                    type: 'send',
-                                },
-                                () => { },
-                            );
+                                    () => { },
+                                );
+                            }
+                            lastMsg = event.message.content;
                         }
-                        lastMsg = event.message.content;
                     }
-                }
-            }
-        })
+                })
 
-        this.currentChannel = location.href.substring(location.href.lastIndexOf("/") + 1, location.href.length);
+            } else if (e.keyCode == 39) {
+                try {
+                    this.cancelPatch();
+                } catch { }
+            }
+        }
+
+
+        
     };
 
     stop() {
-        this.cancelPatch();
+        try {
+            this.cancelPatch();
+        } catch { }
     };
+
+    document() {
+
+    }
 
     //  Initialize
     initialize() {
